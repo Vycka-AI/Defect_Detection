83c6e5ddab1915acdce3e9a13a2d918fdb86bb1e
Vycka-AI <58237473+Vycka-AI@users.noreply.github.com>
2022-12-19 19:53:04 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -10,41 +10,6 @@ from utils.model import CustomVGG
 from utils.helper import train, evaluate, predict_localize
 from utils.constants import NEG_CLASS
 
-print("Imprted libraries")
-data_folder = "hazelnut"
-print(f"Data folder {data_folder}")
-
-batch_size = 10
-target_train_accuracy = 0.98
-lr = 0.0001
-epochs = 10
-class_weight = [1, 3] if NEG_CLASS == 1 else [3, 1]
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Mokymui naudojamas: {device}")
-#print(device)
-heatmap_thres = 0.7
-n_cv_folds = 5
-
-train_loader, test_loader = get_train_test_loaders(
-    root=data_folder, batch_size=batch_size, test_size=0.2, random_state=42,
-)
-
-
-model = CustomVGG()
-
-class_weight = torch.tensor(class_weight).type(torch.FloatTensor).to(device)
-criterion = nn.CrossEntropyLoss(weight=class_weight)
-optimizer = optim.Adam(model.parameters(), lr=lr)
-
-model = train(
-    train_loader, model, optimizer, criterion, epochs, device, target_train_accuracy
-)
-
-model_path = "weights/Modeliukas.h5"
-torch.save(model, model_path)
-# model = torch.load(model_path, map_location=device)
-
-
 
 
 def train(
@@ -87,3 +52,38 @@ def train(
                 break
 
     return model
+
+if __name__ == "__main__":
+    print("Imprted libraries")
+    data_folder = "hazelnut"
+    print(f"Data folder {data_folder}")
+
+    batch_size = 10
+    target_train_accuracy = 0.98
+    lr = 0.0001
+    epochs = 10
+    class_weight = [1, 3] if NEG_CLASS == 1 else [3, 1]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Mokymui naudojamas: {device}")
+    #print(device)
+    heatmap_thres = 0.7
+    n_cv_folds = 5
+
+    train_loader, test_loader = get_train_test_loaders(
+        root=data_folder, batch_size=batch_size, test_size=0.2, random_state=42,
+    )
+
+
+    model = CustomVGG()
+
+    class_weight = torch.tensor(class_weight).type(torch.FloatTensor).to(device)
+    criterion = nn.CrossEntropyLoss(weight=class_weight)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    model = train(
+        train_loader, model, optimizer, criterion, epochs, device, target_train_accuracy
+    )
+
+    model_path = "weights/Modeliukas.h5"
+    torch.save(model, model_path)
+    # model = torch.load(model_path, map_location=device)
